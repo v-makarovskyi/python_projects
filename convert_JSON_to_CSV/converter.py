c723cfe17646b3500665ec601f08667b602810cd
Vladimir <bigcorvet@gmail.com>
2022-11-18 16:23:28 +0200

    first commit

--- convert_JSON_to_CSV/converter.py
+++ convert_JSON_to_CSV/converter.py
@@ -2,14 +2,14 @@ import json
 
 if __name__ == '__main__':
     try:
-        with open('input.json', 'r') as f:
+        with open('/Users/vladimir/Документы/IT/python_projects/convert_JSON_to_CSV/input.json', 'r') as f:
             data = json.loads(f.read())
 
         output = ','.join([*data[0]])
         for obj in data:
             output += f'\n{obj["Name"]},{obj["age"]},{obj["birthyear"]}'
 
-        with open('output.csv', 'w') as f:
+        with open('/Users/vladimir/Документы/IT/python_projects/convert_JSON_to_CSV/output.csv', 'w') as f:
             f.write(output)
     
     except Exception as ex:
