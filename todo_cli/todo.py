149781e85170e69f90f9cd302ebeb0578c2499e5
Vladimir <bigcorvet@gmail.com>
2022-11-24 09:55:51 +0200

    add def 'add'

--- todo_cli/todo.py
+++ todo_cli/todo.py
@@ -23,3 +23,18 @@ def tasks(ctx):
         click.echo('')
     else:
         click.echo('Пока нет задач! Используйте ADD, чтобы добавить первую.\n')
+
+@todo.command()
+@click.pass_context
+@click.option('-add', '--add_task', prompt='Введите задачу для добавления')
+def add(ctx, add_task):
+    """ Добавить задачу """
+    if add_task:
+        #Добавить задачу в список в контексте
+        ctx.obj['TASKS'][ctx.obj['LATEST']] = add_task
+        click.echo('Добавлена задача "' + add_task + '" с ID ' + str(ctx.obj['LATEST']))
+        #Открыть todo.txt и записать текущий индекс и задачи с идентификаторами (разделенные " ```")
+        curr_ind = [str(ctx.obj['LATEST'] + 1)]
+        tasks = [str(i) + '````' + t for (i, t) in ctx.obj['TASKS'].items()]
+        with open('./todo.txt', 'w') as f:
+            f.writelines(['%s\n' % en for en in curr_ind + tasks])
